280ee8776c4539e31782482427771f96855221d4
Paulina Sadowska <7paulina.sadowska@gmail.com>
2018-04-29 13:51:05 +0200

    add firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyADS3O-8X-3qos1p9Zo8bqCElieAxQtlpA',
+      authDomain: 'auth-react-demo-d9cc1.firebaseapp.com',
+      databaseURL: 'https://auth-react-demo-d9cc1.firebaseio.com',
+      projectId: 'auth-react-demo-d9cc1',
+      storageBucket: 'auth-react-demo-d9cc1.appspot.com',
+      messagingSenderId: '146348446162'
+    });
+  }
+  
   render() {
     return (
       <View>
+        <Header headerText="Authentication" />
         <Text>App</Text>
       </View>
     );
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -36,4 +36,4 @@ const styles = {
 
 //only the 'root' component use AppRegistry!
 //make component available to other parts of the app
-export default Header;
+export { Header };
