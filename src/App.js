d9774fb594b37fb712efb295776d52bf14ecdd29
Paulina Sadowska <7paulina.sadowska@gmail.com>
2018-04-29 22:34:23 +0200

    create login form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
   componentWillMount() {
@@ -14,12 +15,12 @@ class App extends Component {
       messagingSenderId: '146348446162'
     });
   }
-  
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>App</Text>
+        <LoginForm />
       </View>
     );
   }
